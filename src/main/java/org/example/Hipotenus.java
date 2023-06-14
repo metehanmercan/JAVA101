a58da930adfd7598fe995c465256a2ec253d625d
metehan mercan <metehanmercan>
2023-06-14 14:42:24 +0300

    JAVA101

---
+++ src/main/java/org/example/Hipotenus.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+public class Hipotenus {
+    public static void main(String[] args) {
+
+        double a,b,c;
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("a dik kenarını giriniz:");
+        a=input.nextDouble();
+        System.out.print("b dik kenarını giriniz:" );
+        b= input.nextDouble();
+
+        c=Math.sqrt((a*a)+(b*b));
+        System.out.println("hiponetüz uzunluğu:"+ c);
+
+
+    }
+}
