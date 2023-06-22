cdb6d064618cff4e50c79b3ec60b430c299c0e08
metehan mercan <metehanmercan>
2023-06-22 19:55:31 +0300

    JAVA101

---
+++ src/main/java/org/example/UsluSayiHesaplama.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class UsluSayiHesaplama {
+    public static void main(String[] args) {
+        int a,b,total=1;
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("üssü alınacak sayı:");
+        a= input.nextInt();
+        System.out.println("sayının üssü:");
+        b= input.nextInt();
+
+        for (int i=1;i<=b;i++) {
+            total *= a;
+        }
+        System.out.println("sonuç:"+ total);
+    }
+}
