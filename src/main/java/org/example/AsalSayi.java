b0c81e9c5a5b0fb7befde227da257f5daa5930bc
metehan mercan <metehanmercan>
2023-06-29 23:07:55 +0300

    JAVA101

---
+++ src/main/java/org/example/AsalSayi.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz : ");
+        int n = input.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime=true;
+            for (int m = 2; m <= i - 1; m++) {
+                if(i%m==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
+}
