d6213c251b14680c93207ef9ad322a49fb82afab
metehan mercan <metehanmercan>
2023-07-09 22:53:29 +0300

    JAVA101

---
+++ src/main/java/org/example/AsalSayi2.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AsalSayi2 {
+    static void asal(){
+        Scanner input=new Scanner(System.in);
+        System.out.print("bir sayı giriniz:");
+        int sayi= input.nextInt();
+        int result=0;
+        if(sayi>0){
+        for(int i=1;i<=sayi;i++){
+            if(sayi%i==0){
+               result++;
+            }
+        }
+        if(result<=2){
+            System.out.println("asaldır");
+        }else{
+            System.out.println("asal değildir");
+          }
+        }else {
+            System.out.println("pozitif bir sayı giriniz ");
+        }
+    }
+    public static void main(String[] args) {
+       do {
+           asal();
+       }while (true);
+    }
+}
