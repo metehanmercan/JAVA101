3ccdef26a9e2c4db1ef4fe0ed7cce5ad896affd3
metehan mercan <metehanmercan>
2023-07-25 14:52:57 +0300

    JAVA101

---
+++ src/main/java/org/example/java101/patika/Diziler/FrekansBulma.java
@@ -0,0 +1,37 @@
+package org.example.java101.patika.Diziler;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class FrekansBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("elaman sayısı?: ");
+        int n = input.nextInt();
+        int[] list = new int[n];
+
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(i + 1 + ". eleman");
+            int sayi = input.nextInt();
+            list[i] = sayi;
+        }
+
+        System.out.println(Arrays.toString(list));
+
+
+
+        for (int j = 0; j < list.length; j++) {
+            int plus = 1;
+            for (int k = j + 1; k < list.length; k++) {
+                if (list[j] == list[k]) {
+                    plus++;
+                } else {
+                    plus =1;
+                }
+
+            }
+            System.out.println(list[j] + " sayısının frekansı: " + plus);
+
+        }
+    }
+}
